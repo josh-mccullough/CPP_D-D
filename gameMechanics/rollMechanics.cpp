ce216697518605dfa2543f843200b7115747bbb0
Josh Mccullough <joshmccullough@Joshs-MacBook-Pro.local>
2019-02-09 20:45:52 +0000

    adding folder structure

---
+++ gameMechanics/rollMechanics.cpp
@@ -0,0 +1,25 @@
+#include <random>
+
+int oneRoll()
+{
+	int roll;
+    int min = 1; // the min number a die can roll is 1
+    int max = 6;// this->dieSize; // the max value is the die size
+
+    roll = rand() % (max - min + 1) + min;
+
+    return roll;
+}
+
+
+bool checkHitChance()
+{
+    if (oneRoll() > 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
